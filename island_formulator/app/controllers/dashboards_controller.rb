bb7d6ebe1349688a634f68f94bc56122e1fef506
Ashley Williams <williamsashley851@gmail.com>
2026-03-19 19:35:36 -0500

    Remove favorites from dashboard

--- island_formulator/app/controllers/dashboards_controller.rb
+++ island_formulator/app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def show
     @recent_recipes = current_user.recipes.order(created_at: :desc).limit(5)
     @recent_batches = current_user.batches.includes(:recipe).order(made_on: :desc).limit(5)
-    @favorite_recipes = current_user.favorited_recipes.order(created_at: :desc).limit(5)
+    #@favorite_recipes = current_user.favorited_recipes.order(created_at: :desc).limit(5)
     
     @stats = {
       ingredients: current_user.ingredients.count,
